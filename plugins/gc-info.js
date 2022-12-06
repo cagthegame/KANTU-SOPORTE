70567c5cd10be8b35a001031d3992192e0936072
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-12-05 23:08:49 -0600

    💫 𝐌𝐘𝐒𝐓𝐈𝐂 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 💫

--- plugins/gc-info.js
+++ plugins/gc-info.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, groupMetadata }) => {
-const { antiToxic, antiTraba, antiviewonce, isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, antiLink2, modohorny, autosticker, modoadmin, audios, delete: del } = global.db.data.chats[m.chat]
+const { antiArab, antiToxic, antiTraba, antiviewonce, isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, antiLink2, modohorny, autosticker, modoadmin, audios, delete: del } = global.db.data.chats[m.chat]
 let text = `*「 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎 」*
 
 *𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂 𝙰𝚄𝚃𝙾𝙼𝙰𝚃𝙸𝙲𝙰𝚂:*
@@ -9,6 +9,7 @@ let text = `*「 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍 𝐃𝐄𝐋 𝐆�
 —◉ 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺 𝟸: ${antiLink2 ? '✅' : '❌'} 
 —◉ 𝙰𝙽𝚃𝙸𝚃𝚁𝙰𝙱𝙰: ${antiTraba ? '✅' : '❌'} 
 —◉ 𝙼𝙾𝙳𝙾𝙰𝙳𝙼𝙸𝙽: ${modoadmin ? '✅' : '❌'} 
+—◉ 𝙰𝙽𝚃𝙸𝙰𝚁𝙰𝙱𝙴𝚂: ${antiArab ? '✅' : '❌'} 
 `.trim()
 m.reply(text)}
 handler.help = ['infogrup']
