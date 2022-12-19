ebda8b553a9f0bfbde842422ca2137f7ef323146
KantuBot <113566411+cagthegame@users.noreply.github.com>
2022-12-19 16:03:36 -0600

    Update info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix }) => {
-let name = '👑 Mystic - Creador 👑'    
-let numero = '5219992095479'
-let numeroformato = '+52 1 999 209 5479'
+let name = '👑 Cris Creador 👑'    
+let numero = '5215591478197'
+let numeroformato = '+52 55 91478197'
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nORG:${name}\nTITLE:\nitem1.TEL;waid=${numero}:${numeroformato}\nitem1.X-ABLabel:${name}\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:${name}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: name, contacts: [{ vcard }] }}, {quoted: m})}
 handler.help = ['owner', 'creator']
