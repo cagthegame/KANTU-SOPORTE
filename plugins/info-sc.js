92c8f8fa3341e66c82c0755f2941e4ea9cfc40be
KantuBot <113566411+cagthegame@users.noreply.github.com>
2022-12-19 16:04:47 -0600

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let texto1 = `*https://github.com/BrunoSobrino/TheMystic-Bot-MD*`
+let texto1 = `*https://github.com/cagthegame/KANTU-BOT*`
 m.reply(texto1)
 }
 handler.command = ['sc','script']
